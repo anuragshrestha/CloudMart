d46e5ab66228e6b028c0402955a09c9e445b6d18
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-10-25 02:12:23 -0600

    created shop context to share all products across the components

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../context/ShopContext' ;
 
-export const ShopCategory = () => {
+export const ShopCategory = (prop) => {
+
+  const {all_product} = useContext(ShopContext);
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      <img src={prop.banner} alt=''/>
+     </div>
   )
-}
+}  
