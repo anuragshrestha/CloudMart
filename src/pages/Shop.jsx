8fc3f6bc8172fe4299171b605cc68c157d18d791
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-10-24 21:27:04 -0600

    mount newcollections and newsletter in shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Home } from '../components/homeScreen/Home'
 import Popular  from '../components/Popular/Popular'
 import { Offers } from '../components/offers/Offers'
+import { NewCollections } from '../components/newCollections/NewCollections'
+import { NewsLetter } from '../components/newsLetter/NewsLetter'
 
 
 export const Shop = () => {
@@ -10,6 +12,8 @@ export const Shop = () => {
       <Home/>
       <Popular/>
       <Offers/>
+       <NewCollections/>
+       <NewsLetter/>
     </div>
   )
 }
