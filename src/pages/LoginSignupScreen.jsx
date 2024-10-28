0b08b69540eed63a691e506a2de297277176fd5c
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-10-27 22:27:42 -0600

    crreated a loginsignup section

--- src/pages/LoginSignupScreen.jsx
+++ src/pages/LoginSignupScreen.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export const LoginSignupScreen = () => {
   return (
-    <div>LoginSignupScreen</div>
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign up</h1>
+     
+      <div className="loginsignup-fields">
+        <input type='text' placeholder='Enter your full name' />
+        <input type='email' placeholder='Enter your email'/>
+        <input  type='password' placeholder='Enter a password' />
+      </div>
+      <button>Continue</button>
+   <p  className="loginsignup-login">
+    Already have an account? <span>Login here</span>
+   </p>
+   <div className="loginsignup-agree">
+    <input type='checkbox' name='' id='' />
+    <p>By continuing I agree to the terms of use & privacy policy.</p>
+   </div>
+      </div>
+    </div>
   )
 }
