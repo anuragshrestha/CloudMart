98d8dadc7a04eba0f36db6d15813eea1adbf60b7
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-09 23:51:56 -0700

    created the description, sizes, category for individual products

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -30,8 +30,25 @@ export const ProductDisplay = (props) => {
           <p>(122)</p>
         </div>
         <div className="productdisplay-right-prices">
-            
+            <div className="productdisplay-right-price-old">${product.old_price}</div>
+            <div className="productdisplay-right-price-new">${product.new_price}</div>
         </div>
+        <div className="productdisplay-right-description">
+          A lightweight, knitted, pullover shirt.
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-size">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className="productdisplay-right-category"> <span>Category :</span>Women, T-Shirt, Crop Top </p>
+        <p className="productdisplay-right-category"> <span>Tags :</span> Modern, Latest </p>
       </div>
     </div>
   );
