3444a95241e4e16c5093f4e57c59373adba98c3b
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-17 15:54:48 -0700

    adjusted the css according to device width

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import "./Navbar.css";
 import logo from "./../assets/logo.png";
 import cart_icon from "./../assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../context/ShopContext";
+import dropdown_icon from './../assets/dropdown_icon.png'
 
 const Navbar = () => {
   //use state to keep track of the curent screen and show border
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems} = useContext(ShopContext);
+  const menuRef = useRef();
 
+  const drop_down = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+ }
+ 
   return (
     <div className="navbar"> 
       <div className="nav-logo">
         <img src={logo} alt="" />
         <p>FASHION</p>
       </div>
-      <ul className="nav-menu">
+      <img className="nav-dropdown" onClick={drop_down} src={dropdown_icon} alt="" />
+      <ul  ref={menuRef} className="nav-menu">
         <li
           onClick={() => {
             setMenu("shop");
