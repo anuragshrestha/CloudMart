90e567b77a0b26141e79c059d8a82218ab908dc2
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-17 14:42:40 -0700

    calculated the total cart amount

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -31,7 +31,19 @@ const ShopContextProvider = (props) =>{
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    const contextValue = {all_product, cartItems, addToCart, removeFromCart};
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+
+        for(const item in cartItems){
+          if(cartItems[item] > 0){
+            let cartInfo = all_product.find((product) => product.id === Number(item));
+            totalAmount += cartInfo.new_price * cartItems[item];
+          }
+        }
+         return totalAmount;
+    }
+
+    const contextValue = {all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount};
     
     return(
         <ShopContext.Provider value = {contextValue} >
