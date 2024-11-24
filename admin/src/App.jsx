1aae3029d79b89de8a97f00fd774ed866f1bec09
Anurag Shrestha <anuragshrestha16@gmail.com>
2024-11-24 16:24:34 -0700

    small update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import Navbar from "./components/navbar/Navbar";
+import Navbar from '../src/components/navbar/Navbar'
 import Admin from "./pages/admin/ Admin";
 
 export const backend_url = 'http://localhost:4000';
@@ -8,12 +8,10 @@ export const currency = '$';
 
 function App() {
   return (
-    <BrowserRouter>
       <div>
         <Navbar />
         <Admin />
       </div>
-    </BrowserRouter>
   );
 }
 
--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import navlogo from '../../assets/nav-logo.svg'
+import product_36 from '../../assets/product_36.png'
 import navprofileIcon from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
